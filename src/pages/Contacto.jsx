93ee644e4a73d67f8b87febccdeeb525a41a5a60
RH-exe <practicante.ti@ecoservicios.pe>
2025-07-08 15:10:38 -0500

    avance final

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,8 +1,10 @@
 import emailjs from 'emailjs-com';
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import Swal from 'sweetalert2';
 
 import img1 from '../assets/img-Contacto.jpg';
+import { Timer } from 'lucide-react';
 
 const UbiPrincipal = { lat: -11.823387402158415, lng: -77.10523711485514 };
 const UbiAdmin = { lat: -11.999923905734665, lng: -77.05825548226619 };
@@ -13,7 +15,13 @@ const nameAdmin = "Av. Industrial N° 3233 - Urb. Industrial Panameric. Norte - 
 
 const Contacto =()=>{
     const form = useRef();
-    const [loading, setLoaging] = useState(false);
+    
+    const [telefono, setTelefono] = useState("");
+    const [dni, setDni] = useState("");
+    const [errorValidar, setErrorValidar] = useState(false);
+    const [mensaje, setMensaje] = useState(false);
+
+    const [loading, setLoading] = useState(false);
 
     const [center, setCenter] = useState(UbiAdmin);
     const [activo, setActivo] = useState("admin");
@@ -35,11 +43,52 @@ const Contacto =()=>{
     };
 
     if (loadError) return <p>Error cargando el mapa 🧨</p>;
+
     if (!isLoaded) return <p>Cargando mapa...</p>;
+
+    const handleChange = (e) => {
+        const value = e.target.value;
+        if (/^\d*$/.test(value)) {
+            setTelefono(value);
+        }
+    };
+
+    const Validar = (e) =>{
+        const value =e.target.value;
+        if(/^\d*$/.test(value)){
+            setDni(value);
+        }
+    };
 
     const enviarCorreo =(e)=>{
         e.preventDefault();
-        setLoaging(true);
+        setLoading(true);
+        
+        if(telefono.length !== 9){
+            Swal.fire({
+                icon: 'error',
+                title: 'Telefono inválido',
+                text: 'El telefono debe tener exactamente 9 dígitos.',
+                showConfirmButton:"false",
+                timer: 2000,
+            });
+            setLoading(false);
+            setErrorValidar(true);
+            return;
+        }
+
+        if(dni.length !== 8){
+            Swal.fire({
+                icon:"error",
+                title:"DNI invalido",
+                text: "El DNI debe tener exactamente 8 digitos",
+                showConfirmButton:"false",
+                timer: 2000,
+            })
+            setLoading(false);
+            setErrorValidar(true);
+            return;
+        }
 
         emailjs.sendForm(
             'service_5yt9hg5',
@@ -48,27 +97,40 @@ const Contacto =()=>{
             'QH-b85TXXiB73J7gM'
         )
         .then(() => {
-            alert('formulario enviado');
+            Swal.fire({
+                title: "Formulario Enviado",
+                icon: "success",
+                showConfirmButton:"false",
+                timer: 2000,
+            });
             form.current.reset();
-            setLoaging(false);
+            setDni("");
+            setTelefono("");
+            setErrorValidar(false);
+            setLoading(false);
         })
-        .catch((error)=> alert('Error al enviar '+ error.text));
-    }
+        .catch((error)=>
+            alert('Error al enviar '+ error.text));
+            setLoading(false);
+    };
+
+    
+    
 
     return(
         <div className='bg-black mt-10'>
                 <div className='flex  justify-center  '>
-                    <div className='grid grid-cols-1 md:grid-cols-[60%_20%] py-[5rem] w-full justify-items-center '>
+                    <div className='grid grid-cols-1 md:grid-cols-[60%_30%] py-[5rem] w-full justify-items-center '>
                         <div className='bg-white rounded-br-[35px] rounded-tl-[35px] '>
                                 <form ref={form} onSubmit={enviarCorreo} className='p-[2rem] max-w-[600px] transition duration-300 ease-in hover:scale-105'>
                                     <h2 className='text-rojo text-[2rem] pb-[1rem] font-bold'>Contactanos</h2>
                                     <div className='grid grid-cols-1 sm:grid-cols-2 gap-4 '>
                                         <input name='name' type='text' placeholder='Nombres' required className='border p-1'/>
                                         <input name='Lastname' type='text' placeholder='Apellidos' required className='border p-1'/>
-                                        <input name='DNI' type='number' onInput={(e) => {e.target.value = e.target.value.slice(0,8);}} maxLength={8} placeholder='DNI' required className='border p-1 '/>
+                                        <input name='DNI' type='text' value={dni}  maxLength={8} placeholder='DNI' onChange={Validar} required className={`border p-1 ${errorValidar ?"border-rojo" : ""}`}/>
                                         <input name='Company' type='text' placeholder='Empresa' required className='border p-1'/>
                                         <input name='address' type='text' placeholder='Dirección/Pais' required className='border p-1'/>
-                                        <input name='Phone' type='number' onInput={(e) => {e.target.value = e.target.value.slice(0,9);}} placeholder='Telefono' required className='border p-1'/>
+                                        <input name='Phone' type='text' placeholder='Telefono' maxLength={9} value={telefono} onChange={handleChange} required className={`border p-1 ${errorValidar ? "border-rojo" : " "}`}/>
                                         <input name='Email_id' type='email' placeholder='Correo' required className='border p-1'/>
                                         <input name='Service' type='text' placeholder='Servicio' required className='border p-1'/>
                                     </div>
@@ -94,7 +156,7 @@ const Contacto =()=>{
                     <div className="relative z-10 flex justify-center gap-10">
                         <button
                         onClick={() => {cambiarMapa("admin"); setNombre(nameAdmin)}}
-                        className={`text-white bg-black hover:bg-rojo rounded-t-md border-l border-t border-r border-white border-b-0 hover:border-b hover:border-white px-6 py-2 transition-all duration-300 ${
+                        className={`text-white bg-black hover:bg-rojo rounded-t-md border-l border-t border-r border-white border-b-0 px-6 py-2 transition-all duration-300 ${
                             activo === "admin" ? "bg-rojo" : ""
                         }`}
                         >
@@ -102,7 +164,7 @@ const Contacto =()=>{
                         </button>
                         <button
                         onClick={() => {cambiarMapa("principal"); setNombre(namePrincipal)}}
-                        className={`text-white bg-black hover:bg-rojo rounded-t-md border-l border-t border-r border-white border-b-0 hover:border-b hover:border-white px-6 py-2 transition-all duration-300 ${
+                        className={`text-white bg-black hover:bg-rojo rounded-t-md border-l border-t border-r border-white border-b-0  px-6 py-2 transition-all duration-300 ${
                             activo === "principal" ? " bg-rojo" : ""
                         }`}
                         >
